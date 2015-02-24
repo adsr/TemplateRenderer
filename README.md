669c1fc0762be5d120f3ef6d9c3e781eed41600f
Adam Saponara <saponara@gmail.com>
2015-02-23 20:44:56 -0500

    docs

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# TemplateRenderer
+TemplateRenderer
+================
+
+TemplateRenderer is a simple templating rendering library for PHP weighing in 
+at under 300 lines of code. It consists of 2 classes (`TemplateRenderer` and 
+`TemplateCompiler`) and 1 script `compile.php` for template compiling.
+
+Features:
+
+ * Templates are plain old PHP
+ * Nested templates
+ * Template inheritance
+ * Easy localization
+
+See `example/` for example usage.
--- example/example.php
+++ example/example.php
@@ -3,16 +3,22 @@
 require dirname(__DIR__) . '/lib/TemplateRenderer.php';
 
 $renderer = new TemplateRenderer(
-    __DIR__ . '/templates',
-    '.php',
-    __DIR__ . '/templates_compiled',
-    __DIR__ . '/templates_strings'
+    __DIR__ . '/templates', // Look for uncompiled templates here
+    '.php', // Templates end with this file extension
+    __DIR__ . '/templates_compiled', // Look for compiled templates here
+    __DIR__ . '/templates_strings' // Look for localized strings here
 );
 
 if (isset($_SERVER['argv'][1])) {
+    // In production, call `setCompiledLang` with the current language code.
+    // This tells TemplateRenderer to use compiled templates + localized
+    // strings instead of the original templates in `templates/`. In dev,
+    // you can skip this step so that changes to files in `templates/` are
+    // reflected immediately without having to run `bin/compile.php`.
     $renderer->setCompiledLang($_SERVER['argv'][1]);
 }
 
+// Render examples/fruits.php with some data
 $renderer->render('fruits', [
     'show_fruits' => true,
     'fruits' => ['apple', 'banana', 'cherry'],
